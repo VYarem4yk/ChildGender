06943ce510a1a5c92b618cb8754d386c8179088b
Vladislav Yaremchuk <VYarem4yk@gmail.com>
2020-05-11 19:32:26 +0300

    Some style changes

--- src/basicData/index.js
+++ src/basicData/index.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from "react";
+import styles from "../index.css";
 class BasicData extends Component {
   constructor(props) {
     super(props);
@@ -6,6 +7,7 @@ class BasicData extends Component {
       fatherBirthday: "",
       motherBirthday: "",
       conceptionDate: "",
+      show: false,
     };
   }
   handleFatherInputChange = (event) => {
@@ -28,11 +30,7 @@ class BasicData extends Component {
 
   handleInputData = (event) => {
     event.preventDefault();
-    console.log(
-      this.state.fatherBirthday,
-      this.state.motherBirthday,
-      this.state.conceptionDate
-    );
+    this.setState({ show: true });
   };
 
   handleBloodChange = () => {
@@ -55,50 +53,56 @@ class BasicData extends Component {
   render() {
     return (
       <Fragment>
-        <h1>Пол ребенка</h1>
-        <form>
-          <labeL for="Bithday">
-            <h3>Дата рождения</h3>
-          </labeL>
-          <p>
-            Папа:{" "}
-            <input
-              type="date"
-              id="fatherBirthday"
-              name="Birthday"
-              value={this.state.fatherBirthday}
-              onChange={this.handleFatherInputChange}
-            ></input>
-          </p>
-          <p>
-            Мама:{" "}
-            <input
-              type="date"
-              id="motherBirthday"
-              name="Birthday"
-              value={this.state.motherBirthday}
-              onChange={this.handleMotherInputChange}
-            ></input>
-          </p>
-          <p>
-            Зачатие:{" "}
-            <input
-              type="date"
-              id="conceptionDate"
-              name="Birthday"
-              value={this.state.conceptionDate}
-              onChange={this.handleConceptionInputChange}
-            ></input>
-          </p>
-          <button type="submit" onClick={this.handleInputData}>
-            Отправить
-          </button>
-        </form>
-        {this.handleBloodChange() == "Девочка" ? (
-          <h1 style={{ color: "red" }}>Девочка</h1>
-        ) : (
-          <h1 style={{ color: "blue" }}>Мальчик</h1>
-        )}
+        <div className="fragment">
+          <h2>Пол ребенка</h2>
+          <form>
+            <labeL for="Bithday">
+              <h3>Дата рождения</h3>
+            </labeL>
+            <p>
+              Папа:{" "}
+              <input
+                type="date"
+                id="fatherBirthday"
+                name="Birthday"
+                value={this.state.fatherBirthday}
+                onChange={this.handleFatherInputChange}
+              ></input>
+            </p>
+            <p>
+              Мама:{" "}
+              <input
+                type="date"
+                id="motherBirthday"
+                name="Birthday"
+                value={this.state.motherBirthday}
+                onChange={this.handleMotherInputChange}
+              ></input>
+            </p>
+            <p>
+              Зачатие:{" "}
+              <input
+                type="date"
+                id="conceptionDate"
+                name="Birthday"
+                value={this.state.conceptionDate}
+                onChange={this.handleConceptionInputChange}
+              ></input>
+            </p>
+            <button type="submit" onClick={this.handleInputData}>
+              Отправить
+            </button>
+          </form>
+          {this.state.show ? (
+            this.handleBloodChange() == "Девочка" ? (
+              <h1 style={{ color: "red" }}>Девочка</h1>
+            ) : (
+              <h1 style={{ color: "blue" }}>Мальчик</h1>
+            )
+          ) : (
+            ""
+          )}
+        </div>
       </Fragment>
     );
   }
