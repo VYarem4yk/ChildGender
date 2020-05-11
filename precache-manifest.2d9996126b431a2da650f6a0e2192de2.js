96719e04a9a30e36fce841ea4d846226c4d28cee
Vladislav Yaremchuk <VYarem4yk@gmail.com>
2020-05-11 19:01:14 +0300

    Updates

---
+++ precache-manifest.2d9996126b431a2da650f6a0e2192de2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc90e3832aa58768de5773f897902c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3768afaeb0e71fa103a1",
+    "url": "/static/css/main.11a2c170.chunk.css"
+  },
+  {
+    "revision": "00ff974ff96ede4f9cfc",
+    "url": "/static/js/2.8b4061e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8b4061e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3768afaeb0e71fa103a1",
+    "url": "/static/js/main.99aa35a9.chunk.js"
+  },
+  {
+    "revision": "bce3412c980c0ca34dad",
+    "url": "/static/js/runtime-main.7590d18b.js"
+  }
+]);
